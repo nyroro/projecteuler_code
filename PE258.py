f013f067b7235646fdfb8346e2b8cc09d6aa93ee
nyroro <nyroro266@gmail.com>
2016-05-07 21:25:54 +0800

    Revert "2016-5-7"
    This reverts commit 5f6c2bc18cb3ed60514cdc512e72bd3523058a28.

--- PE258.py
+++
@@ -1,9 +0,0 @@
-arr = [1]*2000
-
-k=2000
-while True:
-    t=arr[0]+arr[1]
-    arr.pop(0)
-    arr.append(t)
-    print k,t
-    k+=1
--- SelectProblem.py
+++ SelectProblem.py
@@ -1,9 +1,9 @@
 <<<<<<< HEAD
 import random
-l=[266,401,407,451,458,179,160,171,271,272,52,258]
+l=[266,401,407,451,458,179,160]
 
 while 1 :
-    t=random.randint(21,556)
+    t=random.randint(13,468)
     if l.count(t)==0:
         print(t)
         break
